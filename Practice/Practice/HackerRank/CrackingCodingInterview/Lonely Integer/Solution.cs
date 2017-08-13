e78aea846d20c4d4a5affab8e3764d643790ff34
kushal <kushal.bhola22@gmail.com>
2017-08-13 16:55:20 -0400

    Few more problems, at 70% complete

---
+++ Practice/Practice/HackerRank/CrackingCodingInterview/Lonely Integer/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.CrackingCodingInterview.Lonely_Integer
+{
+	class Solution
+	{
+		static void Main(String[] args)
+		{
+			//int n = Convert.ToInt32(Console.ReadLine());
+			//string[] a_temp = Console.ReadLine().Split(' ');
+			//int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+			int[] a = { 0, 0, 1, 2, 1 };
+			Dictionary<int, int> hashMap = new Dictionary<int, int>();
+			for (int i = 0; i < a.Length; i++)
+			{
+				if (hashMap.ContainsKey(a[i]))
+				{
+					hashMap[a[i]] = hashMap[a[i]] + 1;
+				}
+				else
+					hashMap.Add(a[i], 1);
+			}
+			foreach(var hash in hashMap)
+			{
+				if(hash.Value %2 != 0)
+				{
+					Console.WriteLine(hash.Key);
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Practice/Practice/HackerRank/CrackingCodingInterview/TimeComplexityPrimality/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.CrackingCodingInterview.TimeComplexityPrimality
+{
+	class Solution
+	{
+		static void Main(String[] args)
+		{
+			int p = Convert.ToInt32(Console.ReadLine());
+			for (int a0 = 0; a0 < p; a0++)
+			{
+				int n = Convert.ToInt32(Console.ReadLine());
+
+				if (CheckPrime2(n))
+					Console.WriteLine("Not prime");
+				else
+					Console.WriteLine("Prime");
+			}
+			Console.ReadKey();
+		}
+		static bool CheckPrime(int n)
+		{
+			if (n == 2)
+				return true;
+			else if (n % 2 == 0 && n != 2)
+				return false;
+			else
+			{
+				for (int i = 3; i <= n; i = i + 2)
+				{
+					if (n % i == 0 && i!=n)
+						return false;
+				}
+			}
+			return true;
+		}
+		static bool CheckPrime2(int n)
+		{
+			int j = (int)Math.Sqrt(n);
+			if (n == 1)
+				return false;
+			else
+			{
+				for(int i = 2; i <= j; ++i)
+				{
+					if(n%i == 0)
+					{
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+	}
+}
