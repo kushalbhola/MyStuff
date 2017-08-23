c455163659bc94baba3188bddbfb88b794d38fd6
kushal <kushal.bhola22@gmail.com>
2017-08-23 08:23:42 -0400

    big tree class , with many tree problems

---
+++ Practice/Practice/HackerRank/10DaysOfStatistics/MeanMedianMode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.HackerRank._10DaysOfStatistics
+{
+	class MeanMedianMode
+	{
+		static void Main(String[] args)
+		{
+			int N = Convert.ToInt32(Console.ReadLine());
+			//int[] X = new int[N];
+			string[] a_temp = Console.ReadLine().Split(' ');
+			int[] X = Array.ConvertAll(a_temp, Int32.Parse);
+			//for (int i = 0; i < X.Length; i++)
+			//{
+			//	X[i] = Convert.ToInt32(Console.ReadLine());
+			//}
+			//int[] X = { 64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060 };
+
+
+			Console.WriteLine(findMean(X));
+			Console.WriteLine(findMedian(X));
+			Console.WriteLine(findMode(X));
+			Console.ReadKey();
+
+
+		}
+		static double findMean(int[] X)
+		{
+			double mean = 0.0;
+			double sum = 0.0;
+			for (int i = 0; i < X.Length; i++)
+			{
+				sum = sum + X[i];
+			}
+			mean =  sum / X.Length;
+			return mean;
+		}
+		static double findMedian(int[] X)
+		{
+			double median = 0.0;
+			int[] sortedArray = (int[])X.Clone();
+			int Len = sortedArray.Length;
+			Array.Sort(sortedArray);
+			if (sortedArray.Length % 2 != 0)
+				median = sortedArray[sortedArray.Length / 2];
+			else
+			{
+				median = (double)(sortedArray[(Len / 2) - 1] + sortedArray[(Len / 2)]) / 2;
+			}
+			return median;
+		}
+		static int findMode(int[] X)
+		{
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			foreach(var i in X)
+			{
+				if (dict.ContainsKey(i))
+				{
+					dict[i] = dict[i] + 1;
+				}
+				else
+				{
+					dict.Add(i, 1);
+				}
+			}
+			return dict.OrderBy(k => k.Value).ThenBy(o => o.Key).ToDictionary(pair => pair.Key, x => x.Value).First().Key;
+		}
+	}
+}
---
+++ Practice/Practice/HackerRank/Algorithms/Tree/HeightOfTree.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.HackerRank.Algorithms.Tree
+{
+	public class HeightOfTree
+	{
+		public static void Main(String[] args)
+		{
+			int[] arr = { 1, 2, 5, 3, 6, 4 };
+			Node root = ConstructTree(arr);
+			int height = GetHeight2(root);
+			Console.WriteLine(height);
+			Console.WriteLine("PreOrder: ");
+			PreOrder(root); Console.WriteLine();
+			Console.WriteLine("InOrder: ");
+			InOrder(root); Console.WriteLine();
+			Console.WriteLine("PostOrder");
+			PostOrder(root); Console.WriteLine();
+			Console.WriteLine("Top View");
+			topView(root);  Console.WriteLine();
+			Console.WriteLine("Level View");
+			levelOrder(root);
+			Console.ReadKey();
+		}
+		public static Node Insert(Node root, int data)
+		{
+			if(root == null)
+				return new Node(data);
+			else
+			{
+				Node curr;
+				if (data < root.data)
+				{
+					curr = Insert(root.left, data);
+					root.left = curr;
+				}
+				else
+				{
+					curr = Insert(root.right, data);
+					root.right = curr;
+				}
+				return root;
+			}
+		}
+		public static Node ConstructTree(int[] arr)
+		{
+			Node root = null;
+			foreach (int a in arr)
+			{
+				root = Insert(root, a);
+			}
+			return root;
+		}
+		public static int GetHeight(Node root)
+		{
+			if (root == null)
+				return -1;
+			else
+			{
+				//int leftHeight = GetHeight(root.left);
+				//int rightHeight = GetHeight(root.right);
+
+				//if (leftHeight > rightHeight)
+				//	return (leftHeight + 1);
+				//else
+				//	return (rightHeight + 1);
+				return 1 + Math.Max(GetHeight(root.left), GetHeight(root.right));
+			}
+		}
+		public static int GetHeight2(Node root)
+		{
+			if (root == null)
+				return -1;
+			else
+			{
+				int leftHeight = GetHeight2(root.left);
+				int rightHeight = GetHeight2(root.right);
+
+				if (leftHeight > rightHeight)
+					return (leftHeight + 1);
+				else
+					return (rightHeight + 1);
+			}
+		}
+		public static void PreOrder(Node root)
+		{
+			//root >> left >> right
+			if (root == null)
+				return;
+			Console.Write(root.data + " ");
+			PreOrder(root.left);
+			PreOrder(root.right);
+		}
+		public static void InOrder(Node root)
+		{
+			//left >> root >> right
+			if (root == null)
+				return;
+
+			InOrder(root.left);
+			Console.Write(root.data + " ");
+			InOrder(root.right);
+		}
+		public static void PostOrder(Node root)
+		{
+			//Left >>  Right >> Root
+			if (root == null)
+				return;
+			PostOrder(root.left);
+			PostOrder(root.right);
+			Console.Write(root.data + " ");
+		}
+		public static void topView(Node root)
+		{
+			//print root's all left children and root's all right children
+			if(root.left != null)
+				topViewLeft(root.left);
+			Console.Write(root.data + " ");
+			if(root.right != null)
+				topViewRight(root.right);
+		}
+		public static void topViewRight(Node root)
+		{
+			Console.Write(root.data + " ");
+			if (root.right != null)
+				topViewRight(root.right);
+		}
+		public static void topViewLeft(Node root)
+		{
+			if (root.left != null)
+				topViewLeft(root.left);
+			Console.Write(root.data + " ");
+		}
+		public static void levelOrder(Node root)
+		{
+			Queue<Node> queue = new Queue<Node>();
+			if (root != null)
+				queue.Enqueue(root);
+			while(queue.Count != 0)
+			{
+				Node d = queue.Dequeue();
+				Console.Write(d.data + " ");
+				if (d.left != null)
+					queue.Enqueue(d.left);
+				if (d.right != null)
+					queue.Enqueue(d.right);
+			}
+		}
+	}
+	public class Node
+	{
+		public Node left;
+		public Node right;
+		public int data;
+		public Node(int data)
+		{
+			this.data = data;
+			left = null;
+			right = null;
+		}
+	}
+}
