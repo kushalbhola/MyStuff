cd08d8bca9b9854a3e48a7d514b0450349ed46f4
kushal <kushal.bhola22@gmail.com>
2018-07-04 13:38:58 -0400

    new Leetcode stuff

---
+++ Practice/Practice/Leetcode/111_Minimum Depth of Binary Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.Leetcode
+{
+    class _111_Minimum_Depth_of_Binary_Tree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            //int minHeight = 0;
+            if (root == null)
+                return 0;
+            else if (root.left == null && root.right != null)
+                return 1 + MinDepth(root.right);
+            else if (root.left != null && root.right == null)
+                return 1 + MinDepth(root.left);
+            else
+                return 1 + Math.Min(MinDepth(root.left), MinDepth(root.right));
+        }
+    }
+}
---
+++ Practice/Practice/SQL/HackerRank/Advanced Joins/Placements.sql
@@ -0,0 +1,8 @@
+﻿--Question: https://www.hackerrank.com/challenges/placements/problem
+
+select s.Name from students s 
+    inner join friends f on s.id = f.id
+    inner join packages fS on fS.ID = f.friend_id
+    inner join packages sS on sS.ID = s.ID
+where sS.Salary < fS.Salary
+order by fs.Salary
